2d1833f65c34f342a0e4ba094ff84e690182fab8
Yanni-B <bourechoucheyanni@gmail.com>
2025-12-22 12:15:22 -0500

    Changement vers java25 // Changement fichier Task.java

--- src/api/src/main/java/com/mariadb/todo/domain/Task.java
+++ src/api/src/main/java/com/mariadb/todo/domain/Task.java
@@ -1,27 +1,50 @@
 package com.mariadb.todo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "tasks")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String title;
+    
     private String description;
     private boolean completed;
+
+    // Constructeur sans argument (requis par JPA)
+    public Task() {
+    }
+
+    // Constructeur avec tous les arguments
+    public Task(Long id, String description, boolean completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    // Getters et Setters manuels
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
